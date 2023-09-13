adc8de3b2949dd782436902df76625a10c3b09f6
Ivan Nikolaev <ivanix8@gmail.com>
2023-09-13 15:17:47 +0400

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
         System.out.println("gv");
+        System.out.println(bean.equals(bean1));
+
     }
 }
